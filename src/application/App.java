304f45570d1b573d7822d92792b68bd6169d8eed
NuG <nfsg.nug@gmail.com>
2017-03-01 22:57:43 +0000

    Introducao da class, do exercicio2 no pacote controller

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,11 @@
 package application;
 
 import controller.Exercicio1;
+import controller.Exercicio2;
 
 public class App {
     public static void main(String [] args){
         new Exercicio1();
+        new Exercicio2();
     }
 }
---
+++ src/controller/Exercicio2.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class Exercicio2 {
+	public Exercicio2(){
+		
+	}
+}
